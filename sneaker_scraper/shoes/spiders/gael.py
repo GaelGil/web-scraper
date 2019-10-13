dbfa9897fde4a0e9f6f8f7c474f996dc7931d80e
Gael Gil <gaelgilbello27@gmail.com>
2019-10-12 19:44:26 -0700

    it works, then it doesnt

--- sneaker_scraper/shoes/spiders/gael.py
+++ sneaker_scraper/shoes/spiders/gael.py
@@ -1,17 +1,31 @@
 base_url = 'https://stockx.com/sneakers/'
 size_url = 'size-' 
-page_url = '/page='
+page_url = '?page='
 URLS = [base_url + size_url + str(i) + '?' + page_url + str(i) for i in range(1,25)]
 
 
 def get_urls():
-    new_url = [base_url + size_url + str(i) for i in range(1, 19)]
+    new_url = [base_url + size_url + str(i) for i in range(5, 11)]
     #all possible urls we are scraping
     all_urls = []
-
+    main = []
     for i in range(len(new_url)):
         main = [new_url[i] + page_url + str(x) for x in range(0,26)]
         all_urls.append(main)
-    return all_urls
+
+    for x in range(len(all_urls)):
+        for y in range(len(all_urls[x])):
+            if (all_urls[x][y]) in main:
+                pass
+            elif (all_urls[x][y]) not in main:
+                main.append(all_urls[x][y])
+
+    return main
 
 all_urls = get_urls()
+
+for i in range(len(all_urls)):
+    print(all_urls[i])
+
+
+print(len(all_urls))
--- sneaker_scraper/shoes/spiders/qoutes.py
+++ sneaker_scraper/shoes/spiders/qoutes.py
@@ -5,17 +5,17 @@ from scrapy.http import Request
 #The base url has the main url we scrape from
 #URLS is a list with the same url but with pages
 #1-25 so we can scrape  those pages
-base_url = 'https://stockx.com/sneakers/page='
+base_url = 'https://stockx.com/sneakers?page='
 URLS = [base_url + str(i) for i in range(1, 25)]
 
 class StockxSpider(scrapy.Spider):
     name = 'stockx'
     page_number = 2
     start_urls = [
-        'https://stockx.com/sneakers',
+        'https://stockx.com/sneakers'
     ]
-    for i in range(len(URLS)):
-        start_urls.append(URLS[i])  
+    # for i in range(len(URLS)):
+    #     start_urls.append(URLS[i])  
 
     def parse(self, response):
         items = ShoesItem()
@@ -29,6 +29,19 @@ class StockxSpider(scrapy.Spider):
             items['price'] = shoe_price
 
             yield items
+        next_page = 'https://stockx.com/sneakers?page=' + str(StockxSpider.page_number) + '/'
+        if StockxSpider.page_number <= 25:
+            StockxSpider.page_number += 1
+            for i in range(len(URLS)):
+                yield scrapy.Request(URLS[i])
+        # next_page = response.css('ul.dyMfjb  a::attr(href)').extract_first()
+
+        # if next_page is not None:
+            # base_url = 'https://stockx.com/sneakers?page='
+            # URLS = [base_url + str(i) for i in range(1, 25)]
+        #     for i in range(len(URLS)):
+        #         yield scrapy.Request(URLS[i], call_back=self.parse)
+            # yield scrapy.Request(response.urljoin(next_page))
 
 class GoatSpider(scrapy.Spider):
     name = 'goat_shoes'
