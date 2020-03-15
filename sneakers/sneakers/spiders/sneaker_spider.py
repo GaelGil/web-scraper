ccbf2d093c2c10624f157bf99945011eccacba77
Gael Gil <gaelgilbello27@gmail.com>
2020-03-15 13:28:56 -0700

    working note book

--- sneakers/sneakers/spiders/sneaker_spider.py
+++ sneakers/sneakers/spiders/sneaker_spider.py
@@ -9,23 +9,27 @@ class SneakerSpiderSpider(scrapy.Spider):
     page_number = 2
     start_urls = [
         # 'https://stockx.com/sneakers/last-sale',
-        'https://stockx.com/sneakers/last-sale?years=2019&page=1'
+        # 'https://stockx.com/sneakers/last-sale?years=2019&page=1'
+        'https://stockx.com/air-jordan-11-retro-playoffs-2019'
         ]
 
     def parse(self, response):
-        sneakers = response.css('.browse-tile')
-        for sneaker in sneakers:
-            sneaker_name = sneaker.css('.hbRDwP , .hbRDwP div').css('::text').extract()
-            sneaker_last_sale = sneaker.css('.change::text').extract()
+        row = response.css('tr:nth-child(1) td').css('::text').extract()
+        print(row)
+        yield
+        # sneakers = response.css('.browse-tile')
+        # for sneaker in sneakers:
+        #     sneaker_name = sneaker.css('.hbRDwP , .hbRDwP div').css('::text').extract()
+        #     sneaker_last_sale = sneaker.css('.change::text').extract()
 
 
-            items = SneakersItem()
-            items['sneaker_name'] = sneaker_name
-            items['sneaker_last_sale'] = sneaker_last_sale
+        #     items = SneakersItem()
+        #     items['sneaker_name'] = sneaker_name
+        #     items['sneaker_last_sale'] = sneaker_last_sale
 
-            yield items
+        #     yield items
 
-        next_page = 'https://stockx.com/sneakers/last-sale?years=2019&page=' + str(SneakerSpiderSpider.page_number) + ''
-        if SneakerSpiderSpider.page_number <= 25:
-            SneakerSpiderSpider.page_number += 1
-            yield response.follow(next_page, callback=self.parse)
+        # next_page = 'https://stockx.com/sneakers/last-sale?years=2019&page=' + str(SneakerSpiderSpider.page_number) + ''
+        # if SneakerSpiderSpider.page_number <= 25:
+        #     SneakerSpiderSpider.page_number += 1
+        #     yield response.follow(next_page, callback=self.parse)
