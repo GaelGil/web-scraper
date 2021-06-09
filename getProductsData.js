f4f9027a8a0574bf687905a914eda225b40922ef
Gael Gil <gaelgilbello27@gmail.com>
2021-06-09 11:54:37 -0700

    worked on stuff

--- getProductsData.js
+++ getProductsData.js
@@ -2,71 +2,51 @@ const SneaksAPI = require('./sneaks-api/controllers/sneaks.controllers.js');
 const sneaks = new SneaksAPI();
 const fs = require('fs');
 
-function getPrices(product) {
-    while (product) {
-        // console.log(product.shoeName)
-        // console.log(product.brand)
-        // console.log(product.silhoutte)
-        // console.log(product.styleID)
-        // console.log(product.make)
-        // console.log(product.colorway)
-        // console.log(product.retailPrice)
-        // console.log(product.thumbnail)
-        // console.log(product.releaseDate)
-        var sizes = {}
-        if (product.resellPrices){
-            for (const [key, value] of Object.entries(product.resellPrices)) {
-                for (const [sec_key, sec_value] of Object.entries(product.resellPrices[key])) {
-                    if (!(sec_key in sizes)) {
-                        sizes[sec_key] = sec_value;
-                        }else {
-                            size[sec_key] += sec_value
-                        }
-                            // console.log(sec_key, sec_value);
 
-                }   
+sneaks.getProductPrices("FY2903", function(err, product){
+    if (err) {
+        console.log(error);
+      } 
+    else {
+        while (product) {
+        console.log(product.shoeName)
+        console.log(product.brand)
+        console.log(product.silhoutte)
+        console.log(product.styleID)
+        console.log(product.make)
+        console.log(product.colorway)
+        console.log(product.retailPrice)
+        console.log(product.thumbnail)
+        console.log(product.releaseDate)
+        var sizes = {} // dict to hold the sizes and price
+        let counter = 0; // counter to hold how many sites we have data for
+        // check if we have data
+        if (product.resellPrices){
+            // loop through all the sites
+            for (const [key, value] of Object.entries(product.resellPrices)) {
+                // check if the site is in the the json dict
+                if (product.resellPrices[key]){
+                    counter += 1 
+                    // loop through each sites size and add pricing to sizes dictionary
+                    for (const [sec_key, sec_value] of Object.entries(product.resellPrices[key])) {
+                        if (!(sec_key in sizes)) {
+                            sizes[sec_key] = sec_value;
+                        }else {
+                            sizes[sec_key] += sec_value;
+                        }
+                    }   
+                }
                 console.log(' ');
             }
         }
-        product = false
-    }
-    console.log(sizes)
-}
-
-
-function get_product_details() {
-    sneaks.getProductPrices("FY2903", function(err, product){
-        if (err) {
-            // console.log(error);
-          } 
-        else {
-            getPrices(product);
+        // divide all prices by counter (how many sites we checked)
+        for (const [key, value] of Object.entries(sizes)){
+            sizes[key] = value/counter;
         }
-    });
-}
+        console.log(sizes);
+        // end loop
+        product = false
+        }
+    }
 
-get_product_details();
-
-// function searchQueries(data) {
-//     for (let i = 0; i < data.length; i++) {
-//         sneaks.getProducts(data[i], function(error, products) {
-//             // show error if it occurs
-//             if (error) {
-//             //   console.log(error);
-//             } else {
-//               // get every product
-//               for (let x =0; x < products.length; x++) {
-//                 product = products[x]; // select a product
-//                 const stlye_id = product.styleID;
-//                 while(product) {
-//                     get_product_details(stlye_id)
-//                     product = false;
-//                 } 
-//               }
-//             }
-//         });
-//     }
-// }
-
-// const list_of_queries = ['jordan 1', 'jordan', 'nike', 'adidas', 'yeezy', 'off white', 'jordan 1 high 85']
-// searchQueries(list_of_queries);
+});
