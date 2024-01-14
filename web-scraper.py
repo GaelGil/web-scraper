2915e5d6d8ec8ec2de8e61df6140849109dcfacf
Gael <gaelgilbello27@gmail.com>
2024-01-13 23:25:40 -0800

    created new webscraper

--- web-scraper.py
+++ web-scraper.py
@@ -29,9 +29,12 @@ class Scraper:
         Add songs in a list to a spotify playlists.
     """
 
-    URL = f'https://www.barnesandnoble.com/s/'
-    HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
-
+    url = ''
+    urls = ''
+    headers = ''
+    query = ''
+    link_tag = ''
+    
     def __init__(self):
         """
         This function will call the class function `auth_spotify` and create a list for later use.
@@ -44,40 +47,55 @@ class Scraper:
         -------
         None
         """
-        self.spotify_client = self.auth_spotify()
+
+    def set_url(self, url):
+        self.url = url
+
+    def set_query(self, query):
+        self.query = query
+    
+    def set_header(self, header):
+        self.headers = header
+    
+    def set_urls(self, urls):
+        self.urls = urls
+    
+    def set_link_tag(self, link_tag):
+        self.link_tag = link_tag
+
+    def get_lists_of_links(self):
+        try:
+            response = requests.get(self.url+self.query, headers=self.headers)
+            # console.log(re)
+            response.raise_for_status()
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.content, 'html.parser')
+                book_links = [selector['href'] for selector in soup.select(self.link_tag)]
+                return book_links
+        except requests.RequestException as e:
+            print(f"Request failed: {e}")
+
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+        return
+        
+    def print_data(self, data):
+        if data:
+            for idx, title in enumerate(data, start=1):
+                print(f"{idx}. {title}")        
+        else: 
+            print("nothing")
 
 
 
-def get_data(search_query):
-    try:
-        response = requests.get(URL+search_query, headers=HEADERS)
-        response.raise_for_status()
-
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.content, 'html.parser')
-            book_titles = [title.text.strip() for title in soup.select('.product-shelf-title a')]
-            return book_titles
-
-    except requests.RequestException as e:
-        print(f"Request failed: {e}")
-
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-
-    return None
-
-search_query = 'cool'
-
+url = f'https://www.barnesandnoble.com/s/'
+header = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
+sc = Scraper()
+sc.set_url(url)
+sc.set_header(header)
+sc.set_query("travel")
+sc.set_link_tag('.product-shelf-title a')
 for _ in range(3):  # Making 3 requests as an example
-    barnes_and_noble_data = get_data(search_query)
-
-    if barnes_and_noble_data:
-        # print(barnes_and_noble_data)
-        for idx, title in enumerate(barnes_and_noble_data, start=1):
-            print(f"{idx}. {title}")
-
-    else: 
-        print("nothing")
-    # Add a delay of 2 seconds between requests
+    data = sc.get_lists_of_book_links()
+    sc.print_data(data)
     time.sleep(2)
-
