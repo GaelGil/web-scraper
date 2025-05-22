a5a011b465478c5d8f33d77359317be0b334314f
gaelgil <gaelgilbello27@gmail.com>
2025-05-22 13:21:19 -0700

    working on documentation

--- Scraper.py
+++ Scraper.py
@@ -1,4 +1,4 @@
-"""Proram to scrape a website and the items displayed in it.
+"""Program to scrape a website and the items displayed in it.
 
 Leave one blank line.  The rest of this docstring should contain an
 overall description of the module or program.  Optionally, it may also
@@ -12,7 +12,10 @@ examples.
     xpaths = {
     'title': "title_xpath]",
     }
+    sc.set_xpaths(xpaths)
     sc.scrape_items(data_to_scrape)
+    formated_data = sc.format_data()
+    c.to_csv('example.csv', formated_data)
 """
 
 
@@ -31,11 +34,13 @@ class Scraper:
 
     Attributes:
         driver : The browser driver needed for webscraping
-
         links : None
-            A list of links to scrape
+            Will be used as a list of links to scrape
+        x_paths: None
+            Will be used as a dictionary where the element is a string a (key) and
+            the xpath is the value.
         data : list
-            The data we scraped
+            The data we scraped 
 
     Methods:
         set_url(self, url:str)
@@ -44,6 +49,8 @@ class Scraper:
         scrape_links(self, playlist_ids:str, popularity:int, popular:bool):
             Get a list of tracks for the new playlist.
 
+        set_xpaths(self, )
+        
         scrape_items(self, playlist_ids:str, popularity:int, popular:bool):
             Get a list of tracks for the new playlist.
 
@@ -79,6 +86,9 @@ class Scraper:
     def set_url(self, url: str)  -> None:
         """Function to set the url that we will scrape.
 
+        Function to set the url that we want our scraper to get. After its done
+        we wait for 3 seconds. 
+
         Args:
             url: The url we want to scrape
 
@@ -88,11 +98,13 @@ class Scraper:
         self.driver.get(url)
         time.sleep(3)
 
-    def get_links(self, xpath: str) -> None:
-        """This function gets book titles and their link
+    def scrape_links(self, xpath: str) -> None:
+        """This function scrapes links from a website
 
-        Using the url set previously we scrape links of items on that website
-        we then put those links into a list to be used for individual scraping.
+        Using the url we set previously, we scrape links off of items on that website. 
+        As an example it can be used on the products page of amazon or any website where
+        there are itmems listed. We scrape the links and then put them into the class
+        list `links`.
 
         Args:
             xpath: The xpath to the link elemnts we want to scrape
@@ -107,28 +119,30 @@ class Scraper:
         except Exception as e:
             print('Error while scraping:', e)
 
-    def set_xpaths(self, x_paths: dict) -> None:
-        """This function gets book titles and their link
+    def set_xpaths(self, xpaths: dict) -> None:
+        """This function sets the xpaths for items we want to scrape
 
-        Using the url set previously we scrape links of items on that website
-        we then put those links into a list to be used for individual scraping.
+        This functions sets the class variable xpaths. We are setting
+        a dictionary where the element is a string a (key) and the xpath 
+        is the value. This will be used to scrape each indiviual items
+        data. If we already set xpaths we return and print a message.
 
         Args:
-            xpath: The xpath to the link elemnts we want to scrape
+            xpath: A dictionary where the element is a string a (key) and
+            the xpath is the value.
 
         Returns: 
             None
         """
-        if self.x_paths:
+        if self.xpaths:
             print('x_paths already set, add them instead')
             return 
-        self.x_paths = x_paths
+        self.xpaths = xpaths
 
-    def add_xpath(self, name: str, x_path: str) -> None:
-        """This function gets book titles and their link
+    def add_xpath(self, name: str, xpath: str) -> None:
+        """This function adds a key and value to the xpath dictionary
 
-        Using the url set previously we scrape links of items on that website
-        we then put those links into a list to be used for individual scraping.
+        This function performs a simple
 
         Args:
             xpath: The xpath to the link elemnts we want to scrape
@@ -136,7 +150,7 @@ class Scraper:
         Returns: 
             None
         """
-        self.x_paths[name] = xpath
+        self.xpaths[name] = xpath
         print(f'added {name}')
     
     def scrape_items(self, multiple: list) -> None:
@@ -157,8 +171,8 @@ class Scraper:
         i = 0
         for link in self.links:
             self.set_url(link)
-            item = self.x_paths.copy()
-            for key, xpath in self.x_paths.items():
+            item = self.xpaths.copy()
+            for key, xpath in self.xpaths.items():
                 try:
                     if key in multiple:
                         elements = self.driver.find_elements(By.XPATH, xpath)
@@ -219,7 +233,7 @@ class Scraper:
         Returns:
             None
         """
-        formated_data = [list(self.x_paths.keys())]
+        formated_data = [list(self.xpaths.keys())]
         for value in self.data.values():
             formated_data.append(list(value.values()))        
         return formated_data
@@ -229,8 +243,7 @@ class Scraper:
 # TODO: switch to scraping goodreads instead
 sc = Scraper()
 sc.set_url('https://www.barnesandnoble.com/s/education')
-# TODO: add documentation for set_x_path functions/add xpath
-sc.get_links("//div[@class='product-shelf-title product-info-title pt-xs']/a")
+sc.scrape_links("//div[@class='product-shelf-title product-info-title pt-xs']/a")
 data_to_scrape = {
     'title': "/html/body/main/div[3]/div[2]/section/div[2]/div/div[3]/div[1]/header/div/h1",
     'publish_date' : "//table[@class='plain centered']//tr[th='Publication date:']/td",
