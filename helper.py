9265c58c3f044ab9950b9e07b0f020ab26f0e735
gaelgil <gaelgilbello27@gmail.com>
2025-05-26 19:04:52 -0700

    adding to helper function

--- helper.py
+++ helper.py
@@ -28,5 +28,23 @@ GENRES = [
 'Young Adult',
 
 ]
+
+XPATHS = {
+    'title': "//*[@id='__next']/div[2]/main/div[1]/div[2]/div[2]/div[1]/div[1]/h1",
+    'author' : "//*[@id='__next']/div[2]/main/div[1]/div[2]/div[2]/div[2]/div[1]/h3/div/span[1]/a/span[1]",
+    'rating': '//div[@class="RatingStatistics__rating"]',
+    'raitings' : '//span[@data-testid="ratingsCount"]',
+    'reviews' : '//span[@data-testid="reviewsCount"]',
+    'overview' : '//div[@data-testid="description"]//span[@class="Formatted"]',
+    'x': '//ul[@class="CollapsableList"]//span[@class="Button__labelItem"]',
+    'pages' : '//p[@data-testid="pagesFormat"]',
+    'publish_date' : '//p[@data-testid="publicationInfo"]',
+    }
+
+
 def get_genres():
     return GENRES
+
+
+def get_xpaths():
+    return XPATHS
