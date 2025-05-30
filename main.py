9bc1e87e0ac890ab504ac75593b552cfa5b43816
GaelGil <31393159+GaelGil@users.noreply.github.com>
2025-05-30 14:59:55 -0700

    updated main for use

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
-from Scraper import Scraper
-from helper import XPATHS, make_urls
-
-GECKODRIVER_PATH = "./drivers/geckodriver"
-NEXT_PAGE_BUTTON_XPATH = '//a[@class="next_page" and @rel="next"]'
-LINK_XPATH = "//*[@id='bodycontainer']/div[3]/div[1]/div[2]/div[2]/table/tbody/tr/td[2]/a"
-MULTIPLE = {'genres' : 0}
+from DriverManager import DriverManager
+from ScraperSetUp import CONFIG
+from Scrapers.ProductScraper import ProductScraper
+from Scrapers.ProductsScraper import ProductsScraper
 
 
-sc = Scraper(GECKODRIVER_PATH, headless=True)
-# sc.get_categories('https://www.goodreads.com/genres/', '//span[text()="Browse ▾"]', '//ul[contains(@class, "genreList")]//li/a')
-# print(sc.categories)
-# # sc.generate_urls()
-sc.set_next_page_xpath(NEXT_PAGE_BUTTON_XPATH)
-sc.set_xpaths(XPATHS)
-sc.set_multiple(MULTIPLE)
-sc.set_urls(make_urls())
-sc.set_link_xpath(LINK_XPATH)
-sc.visit_urls(stop=1)
-sc.visit_items()
-formated_data = sc.format_data()
-sc.to_csv('./data/data.csv', formated_data)
+
+if __name__ == "__main__":
+    driver_manager = DriverManager()
+    products_scraper = ProductsScraper(driver=driver_manager, config=CONFIG)
+    product_scraper = ProductScraper(driver=driver_manager, config=CONFIG)
+    products = products_scraper.iterate_urls()
+    data = products_scraper.iterate_urls(products)
+
+
+    print("Products:", products)
+    print("data", )
