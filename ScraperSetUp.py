21ad11c629112bbcdf32d6bb5d29e4c24325960d
GaelGil <31393159+GaelGil@users.noreply.github.com>
2025-06-03 15:31:11 -0700

    updating a config

--- ScraperSetUp.py
+++ ScraperSetUp.py
@@ -1,11 +1,10 @@
 CONFIG_GOODREADS = {
     'PRODUCT': {
-
             'title': "//*[@id='__next']/div[2]/main/div[1]/div[2]/div[2]/div[1]/div[1]/h1",
             'author' : "//*[@id='__next']/div[2]/main/div[1]/div[2]/div[2]/div[2]/div[1]/h3/div/span[1]/a/span[1]",
             'rating': '//div[@class="RatingStatistics__rating"]',
             'raitings' : '//span[@data-testid="ratingsCount"]',
-            'reviews' : '//span[@data-testid="reviewsCount"]',
+            'reviews' : '//span[@2data-testid="reviewsCount"]',
             'overview' : '//div[@data-testid="description"]//span[@class="Formatted"]',
             'genres': '//ul[@class="CollapsableList"]//span[@class="Button__labelItem"]',
             'pages' : '//p[@data-testid="pagesFormat"]',
@@ -22,28 +21,22 @@ CONFIG_GOODREADS = {
 }
 
 
-CONFIG_SNAKERS = {
+CONFIG_STOCKX = {
     'PRODUCT': {
-        'xpaths': {
-            'title': "//*[@id='__next']/div[2]/main/div[1]/div[2]/div[2]/div[1]/div[1]/h1",
-            'author' : "//*[@id='__next']/div[2]/main/div[1]/div[2]/div[2]/div[2]/div[1]/h3/div/span[1]/a/span[1]",
-            'rating': '//div[@class="RatingStatistics__rating"]',
-            'raitings' : '//span[@data-testid="ratingsCount"]',
-            'reviews' : '//span[@data-testid="reviewsCount"]',
-            'overview' : '//div[@data-testid="description"]//span[@class="Formatted"]',
-            'genres': '//ul[@class="CollapsableList"]//span[@class="Button__labelItem"]',
-            'pages' : '//p[@data-testid="pagesFormat"]',
-            'publish_date' : '//p[@data-testid="publicationInfo"]',
-        }
+        'name': "//*[@id='__next']/div[2]/main/div[1]/div[2]/div[2]/div[1]/div[1]/h1",
+        'img': 'img', 
+        'current_price' : "//*[@id='__next']/div[2]/main/div[1]/div[2]/div[2]/div[2]/div[1]/h3/div/span[1]/a/span[1]",
+        'retail_price': '//div[@class="RatingStatistics__rating"]',
+        'release_data' : '//span[@data-testid="ratingsCount"]',
+        'description' : '//span[@2data-testid="reviewsCount"]',
+        'style' : ''
     },
-    'NEXT_PAGE_BUTTON_XPATH': {
-        'xpath' : '//a[@class="next_page" and @rel="next"]'
-    },
-    'PRODUCTS': {
-        'xpath': '//*[@id="bodycontainer"]/div[3]/div[1]/div[2]/div[2]/table/tbody/tr/td[2]/a'
-    },
-    'MULTIPLE': {'genres' : 0},
+    'NEXT_PAGE_BUTTON_XPATH': '//a[@class="next_page" and @rel="next"]',
+    'PRODUCTS':  '//*[@id="bodycontainer"]/div[3]/div[1]/div[2]/div[2]/table/tbody/tr/td[2]/a',
+    'MULTIPLE': None,
     'URLS' : [],
     'DRIVER_PATH': './drivers/geckodriver',
-    'HEADLESS': True
+    'HEADLESS': True,
+    'CATEGORIES_BUTTON' : '//span[text()="Browse ▾"]',
+    'CATEGORIES' : '//ul[contains(@class, "genreList")]//li/a'
 }
