6fcad9b2db8f3994cabb00c97c54605701881f1d
GaelGil <31393159+GaelGil@users.noreply.github.com>
2025-06-02 23:47:14 -0700

    updating base scraper

--- Scrapers/BaseScraper.py
+++ Scrapers/BaseScraper.py
@@ -1,4 +1,4 @@
-"""Program to scrape a website and the items displayed in it.
+"""This is an abstract class that represents a base web scraper
 
 Leave one blank line.  The rest of this docstring should contain an
 overall description of the module or program.  Optionally, it may also
@@ -6,16 +6,7 @@ contain a brief description of exported classes and functions and/or usage
 examples.
 
   Typical usage example:
-    sc = Scraper(driver=driver_path, headless=True)
-    sc.set_next_page_xpath(xpath=next_button_xpath)
-    sc.set_xpaths(xpaths)
-    sc.set_multiple(multiple)
-    sc.set_urls(urls)
-    sc.set_link_xpath(link_xpath)
-    sc.visit_urls(stop=5) # only go to 5 pages per each url
-    sc.visit_items() # visit items scraped
-    formated_data = sc.format_data() # format the data
-    sc.to_csv('./data.csv', formated_data) # write data to csv
+    
 """
 
 from abc import ABC, abstractmethod
@@ -38,22 +29,15 @@ class BaseScraper(ABC):
     classes
 
     Attributes:
-        driver: The browser driver needed for webscraping
-        config: a dictionary representing with important info
-            for the scraper 
+        driver: the browser driver needed for webscraping
+        config: a dictionary that holds info for our webscraper
 
     Methods:
         __init__(self, driver_path: str, headless: bool)
             Initializes the instance to be ready for scraping
 
-        set_url(self, url: str)
+        get_url(self, url: str)
             Function to set the url that we will scrape
-
-        set_next_page_xpath(self, xpath: str)
-            This function sets the xpath for the next page button
-
-        set_xpaths(self, xpaths: dict)
-            This function sets the class variable xpaths
     """
     def __init__(self, driver, config):
         """
