03e6d85543c5733733d9fca587c607858ae55cf5
GaelGil <31393159+GaelGil@users.noreply.github.com>
2025-06-04 00:23:44 -0700

    updated readmne and main,,py

--- README.md
+++ README.md
@@ -50,19 +50,36 @@ CONFIG_GOODREADS = {
 
  ~~~python
 from DriverManager import DriverManager
-from ScraperSetUp import CONFIG_GOODREADS
+from ScraperSetUp import CONFIG
 from Scrapers.ProductScraper import ProductScraper
 from Scrapers.ProductsScraper import ProductsScraper
+from Scrapers.CategoriesScraper import CategoriesScraper
 
-driver_manager = DriverManager(CONFIG_GOODREADS['DRIVER_PATH'], CONFIG_GOODREADS['HEADLESS'])
+# set the driver
+driver_manager = DriverManager(CONFIG_GOODREADS['DRIVER_PATH'], CONFIG_GOODREAD ['HEADLESS'])
+
+# get categories
+categories_scraper = CategoriesScraper(driver=driver_manager, config=CONFIG_GOODREADS)
+categories = categories_scraper.scrape('https://www.goodreads.com/genres')
+    
+# generate urls
+CONFIG_GOODREADS['URLS'] = categories_scraper.generate_urls('https://www.goodreads.com/search?page=99&q=%s&qid=x02cPlELXg&tab=books', categories)
+    
+# get products from page
 products_scraper = ProductsScraper(driver=driver_manager, config=CONFIG_GOODREADS)
+products = products_scraper.iterate_urls(stop=1000, next_page=True, handle_popup=True)
+
+# scrape individual products data
 product_scraper = ProductScraper(driver=driver_manager, config=CONFIG_GOODREADS)
-products = products_scraper.iterate_urls(stop=2, next_page=True, popup=True)
 data = product_scraper.iterate_urls(products)
+
+print(f'Products: {products}')
+print(f'Data: {data}')
+
  ~~~
 
 
 ### Run
 ~~~
-python3 Scraper.py
+python3 main.py
 ~~~
--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from ScraperSetUp import CONFIG_GOODREADS
 from Scrapers.ProductScraper import ProductScraper
 from Scrapers.ProductsScraper import ProductsScraper
 from Scrapers.CategoriesScraper import CategoriesScraper
-
-#TODO: updated readme again
+import csv
 
 if __name__ == "__main__":
     # set the driver
@@ -14,17 +13,25 @@ if __name__ == "__main__":
     categories_scraper = CategoriesScraper(driver=driver_manager, config=CONFIG_GOODREADS)
     categories = categories_scraper.scrape('https://www.goodreads.com/genres')
     # generate urls
-    CONFIG_GOODREADS['URLS'] = categories_scraper.generate_urls('https://www.goodreads.com/search?page=99&q=%s&qid=x02cPlELXg&tab=books', categories)
+    CONFIG_GOODREADS['URLS'] = categories_scraper.generate_urls('https://www.goodreads.com/search?page=1&q=%s&qid=x02cPlELXg&tab=books', categories)
     
     # get products from page
     products_scraper = ProductsScraper(driver=driver_manager, config=CONFIG_GOODREADS)
-    products = products_scraper.iterate_urls(stop=1000, next_page=True, handle_popup=True)
+    products = products_scraper.iterate_urls(stop=3, next_page=True, handle_popup=True)
 
     # # scrape individual products data
     product_scraper = ProductScraper(driver=driver_manager, config=CONFIG_GOODREADS)
     data = product_scraper.iterate_urls(products)
 
-    print(f'Products: {products}')
-    print(f'Data: {data}')
+    formated_data = []
+    for value in data.values():
+        formated_data.append(list(value.get_item_values()))  
 
+    file_name = './data/good_reads.csv'
+    with open(file_name, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(formated_data)
+        print(f'Data written to {file_name}')
 
+    # print(f'Products: {products}')
+    # print(f'Data: {data}')
